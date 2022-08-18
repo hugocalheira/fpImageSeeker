438d9aaf9ae5ab0c365b0cae6145a42a4e7cb05f
Hugo C. Durães <hugocalheira@gmail.com>
2022-08-18 13:31:25 -0300

    locate images from a list

---
+++ fpImagesSeeker.js
@@ -0,0 +1,44 @@
+const promises = require('fs/promises');
+const fs = require('fs');
+
+const fixedPath = "\\\\fsraia01\\APPS\\FPOPULAR\\DROGARAIA\\"
+
+function mountPath({date, key}) {
+    date = new Date(date).toLocaleDateString()
+    const year = date.split('/')[2]
+    const month = date.split('/')[1]
+    const day = date.split('/')[0]
+    const path = `${fixedPath}${year}\\${month}\\${day}`
+    return {path, key}
+}
+
+function listAllFiles({key, path}) {
+    fs.readdirSync(path).forEach(file => {
+        if (file.includes(key)) {
+            copyFile(`${path}\\${file}`, `.\\images\\${file}`)
+        }
+    });
+}
+
+function copyFile(source, target) {
+    fs.copyFile(source, target, (err) => {
+        if (err) throw err;
+        console.log(`${source} was copied...`);
+    });
+}
+
+async function fpImageSeeker() {
+  try {
+    const data = await promises.readFile('fp_autorizacao.csv', { encoding: 'utf8' });
+    const lines = data.split('\n');
+    lines.forEach((line, index) => {
+        if (index > 0 && line.length) {
+            const columns = line.split(',')
+            listAllFiles(mountPath({ key: columns[1], date: columns[7] }))
+        }
+    })
+  } catch (err) {
+    console.log(err);
+  }
+}
+fpImageSeeker();
